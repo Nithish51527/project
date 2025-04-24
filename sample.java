7c4d92ac17f4ebc4b19851d0f5586e9d551a329b
Nithish51527 <nithishmedi018@gmail.com>
2025-04-24 03:40:15 -0700

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,8 +1,37 @@
-public class sample
-{
-	public static void main(strings args[])
-	{
-		system.out.print("Nithish");
-	}
-}
+pipeline {
+    agent any
 
+    stages {
+        stage('Checkout Code') {
+            steps {
+                git branch: 'main', url: https://github.com/Nithish51527/2281A7316.git
+            }
+        }
+        stage('Build') {
+            steps {
+             
+                bat 'javac Sample.java'
+            }
+        }
+        stage('Test') {
+            steps {
+                             bat 'java Sample'
+            }
+        }
+        stage('Deploy') {
+            steps {
+                echo 'Deploying application...'
+                // Add your deployment commands here (like copying files to a server)
+            }
+        }
+Post
+{
+Success
+{
+mail to ‘nithishmedi018@gmail.com’,
+subject:”Build success”,
+Body:’Your build was successfull’
+  }
+}
+   }
+}
