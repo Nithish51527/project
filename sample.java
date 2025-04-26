72b7748f370a6d135b0ec983767d72ecc9582259
Nithish51527 <nithishmedi018@gmail.com>
2025-04-25 23:16:16 -0700

    Update sample.java

--- sample.java
+++ sample.java
@@ -4,7 +4,7 @@ pipeline {
     stages {
         stage('Checkout Code') {
             steps {
-                git branch: 'main', url: https://github.com/Nithish51527/2281A7316.git
+                git branch: 'main', url: 'https://github.com/Nithish51527/2281A7316.git'
             }
         }
         stage('Build') {
